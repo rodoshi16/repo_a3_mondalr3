597b4525d0f8fdaefaed81ee365411a60c3add57
mondalr3 <rodoshi.mondal@mail.utoronto.ca>
2024-11-24 17:26:10 -0500

    Added the comments for Polyline implementation

--- a3/src/main/java/ca/utoronto/utm/paint/PolylineCommand.java
+++ a3/src/main/java/ca/utoronto/utm/paint/PolylineCommand.java
@@ -5,14 +5,17 @@ import javafx.scene.canvas.GraphicsContext;
 import java.util.ArrayList;
 
 public class PolylineCommand extends PaintCommand{
-    // need to store the points where the user clicks to extend a line segment
+    //We need to store the set of points where the user clicks to create the line segments//
     private ArrayList<Point> points=new ArrayList<Point>();
 
+    /*This public class adds the points where the user clicks on the canvas to the list of polyline points. It
+    * then displays the change in the state and notifies observers of the change.*/
     public void add(Point p){
         this.points.add(p);
         this.setChanged();
         this.notifyObservers();
     }
+    /* This public class returns the array of polyline points*/
     public ArrayList<Point> getPoints(){ return this.points; }
 
 
@@ -24,6 +27,7 @@ public class PolylineCommand extends PaintCommand{
         for(int i=0;i<this.points.size()-1;i++){
             Point p1 = this.points.get(i);
             Point p2 = this.points.get(i+1);
+            // the strokeLine method from the GraphicsContext g class automatically does the calculations for the line segment
             g.strokeLine(p1.x, p1.y, p2.x, p2.y);
         }
 
--- a3/src/main/java/ca/utoronto/utm/paint/PolylineManipulatorStrategy.java
+++ a3/src/main/java/ca/utoronto/utm/paint/PolylineManipulatorStrategy.java
@@ -4,7 +4,9 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 
 public class PolylineManipulatorStrategy extends ShapeManipulatorStrategy {
+    // The job of Manipulator strategy class is to create the commands for polyline and add then to the PaintModel
     private PolylineCommand polylineCommand;
+    // We need to track the current point the user is moving the mouse to
     private Point curr;
     PolylineManipulatorStrategy(PaintModel model){
         super(model);
@@ -14,19 +16,23 @@ public class PolylineManipulatorStrategy extends ShapeManipulatorStrategy {
 
     @Override
     public void mouseClicked(MouseEvent e){
-        // getButton method tells us which mouse button was clicked
-        // if its left, get the point and store it
+        // we need to check if a new polyline is being started
         if (this.polylineCommand == null){
+            // polylinecommand is only null when right click occurs
+            // then a new polylinrcommand is initialized and added as a command
             this.polylineCommand = new PolylineCommand();
             this.addCommand(this.polylineCommand);
         }
+        // the point the user has clicked
         Point q = new Point((int)e.getX(),(int) e.getY());
+        // getButton method tells us which mouse button was clicked
+        // if its left, we add the point to the command
         if (e.getButton() == MouseButton.PRIMARY){
             this.polylineCommand.add(q);
             System.out.println("left click");
 
             }
-        // if its right, the end the line
+        // if its right, the end the line by setting it to null
         else if(e.getButton() == MouseButton.SECONDARY){
                 this.polylineCommand = null;
                 //this.curr = null;
@@ -37,12 +43,13 @@ public class PolylineManipulatorStrategy extends ShapeManipulatorStrategy {
 
     @Override
     public void mousePressed(MouseEvent e) {
+        // if right click has not been pressed yet, we the polylinecommand to the paint model
         if(this.polylineCommand != null){
             this.addCommand(polylineCommand);
         }
     }
 
-
+    // polyline has no drag feature
     @Override
     public void mouseDragged(MouseEvent e) {
 
@@ -52,6 +59,7 @@ public class PolylineManipulatorStrategy extends ShapeManipulatorStrategy {
     @Override
     public void mouseMoved(MouseEvent e){
         if(this.polylineCommand != null){
+            // as the mouse moves, the curr is updated according to the mouse points
             this.curr = new Point((int) e.getX(), (int)e.getY());
         }
     }
